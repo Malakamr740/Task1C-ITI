ea50f19b6c86ca5f8270a2ca3dd4bb3f0680a13e
Malakamr740 <116198822+Malakamr740@users.noreply.github.com>
2025-09-12 07:10:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿namespace Task_1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 01
+
+            //Console.Write("Enter a character: ");
+            //string input = Console.ReadLine();
+            //if (!string.IsNullOrEmpty(input))
+            //{
+            //    char character = input[0];
+            //    int ascii = (int)character;
+            //    Console.WriteLine(ascii);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No character was entered");
+            //}
+            #endregion
+
+            #region Question 02
+            //Console.Write("Enter an ASCII code :");
+            //string input = Console.ReadLine();
+
+            //if (int.TryParse(input, out int ascii))
+            //{
+            //    char character = (char)ascii;
+            //    Console.WriteLine(character);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid number");
+            //}
+            #endregion
+
+            #region Question 03
+            //Console.Write("Enter a number: ");
+            //string input = Console.ReadLine();
+
+            //if (int.TryParse(input, out int number))
+            //{
+            //    if (number % 2 == 0)
+            //    {
+            //        Console.WriteLine($"The number {number} is even.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"The number {number} is odd");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid number");
+            //}
+            #endregion
+
+            #region Question 04
+            //Console.Write("Enter the first number:");
+            //string input1 = Console.ReadLine();
+
+            //Console.Write("Enter the second number:");
+            //string input2 = Console.ReadLine();
+
+            //if (double.TryParse(input1, out double num1) && double.TryParse(input2, out double num2))
+            //{
+            //    double sum = num1 + num2;
+            //    double subtraction = num1 - num2;
+            //    double multiplication = num1 * num2;
+
+            //    Console.WriteLine($"Sum: {num1} + {num2} = {sum}");
+            //    Console.WriteLine($"Subtraction: {num1} - {num2} = {subtraction}");
+            //    Console.WriteLine($"Multiplication: {num1} * {num2} = {multiplication}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter valid numbers");
+            //}
+            #endregion
+
+            #region Question 05
+            //Console.Write("Enter the student's score (0-100): ");
+            //string input = Console.ReadLine();
+
+            //if (double.TryParse(input, out double score))
+            //{
+            //    if (score >= 90)
+            //    {
+            //        Console.WriteLine($"The score {score} corresponds to grade 'A'");
+            //    }
+            //    else if (score >= 80)
+            //    {
+            //        Console.WriteLine($"The score {score} corresponds to grade 'B'");
+            //    }
+            //    else if (score >= 70)
+            //    {
+            //        Console.WriteLine($"The score {score} corresponds to grade 'C'");
+            //    }
+            //    else if (score >= 60)
+            //    {
+            //        Console.WriteLine($"The score {score} corresponds to grade 'D'");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"The score {score} corresponds to grade 'F'");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid score.");
+            //}
+            #endregion
+
+            #region Question 06
+            //Console.Write("Enter a number : ");
+            //string input = Console.ReadLine();
+
+            //if (int.TryParse(input, out int number))
+            //{
+            //    Console.WriteLine($"Multiplication table for {number}:");
+            //    for (int i = 1; i <= 10; i++)
+            //    {
+            //        Console.WriteLine($"{number} x {i} = {number * i}");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid number");
+            //}
+            #endregion
+
+        }
+    }
+}
